3cb783baadc89b88048e2b91b370edff5603dc81
rauf-create539 <raufimar44@gmail.com>
2025-09-26 07:52:58 +0800

    Added: models for reports

--- backend/report/models.py
+++ backend/report/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from user_authentication.models import User
 
 # Create your models here.
+class Report(models.Model):
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    reported_user = models.ForeignKey(User, related_name='reports_received', on_delete=models.CASCADE)
+    reason = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
