e7368d285a79f358937da5831f2842c4604a6aac
rauf-create539 <raufimar44@gmail.com>
2025-09-26 07:08:54 +0800

    Added: models for destinations and attractions

--- backend/destinations_and_attractions/models.py
+++ backend/destinations_and_attractions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    CATEGORY_CHOICES = [
+        ('Cultural', 'Cultural'),
+        ('Historical', 'Historical'),
+        ('Adventure', 'Adventure'),
+        ('Nature', 'Nature'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    location = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='destinations/', blank=True, null=True)
+    average_rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+class Attraction(models.Model):
+    destination = models.ForeignKey(Destination, related_name='attraction', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='attractions/', blank=True, null=True)
+    average_rating = models.FloatField(default=0.0)
