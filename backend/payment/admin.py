32ec614a688252eb4fc06d853777a7496c1e3b02
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 12:37:41 -0700

    Added: custom admin for payment admin

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Payment
 
 # Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payer', 'payment_type', 'amount', 'service_fee', 'payment_method', 'timestamp')
+    list_filter = ('payment_type', 'payment_method', 'timestamp')
+    search_fields = ('payer__username', 'gcash_transaction_id')
+
+admin.site.register(Payment, PaymentAdmin)
