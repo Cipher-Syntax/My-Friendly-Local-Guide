450baa7ba1422657380ed9597a3372513422c3b7
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 12:21:18 -0700

    Added: custom destination & attraction admin

--- backend/destinations_and_attractions/admin.py
+++ backend/destinations_and_attractions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Destination, Attraction
 
 # Register your models here.
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'location', 'average_rating')
+    list_filter = ('category', 'location')
+    search_fields = ('name', 'location')
+
+admin.site.register(Destination, DestinationAdmin)
+
+class AttractionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'destination', 'average_rating')
+    list_filter = ('destination',)
+    search_fields = ('name',)
+
+admin.site.register(Attraction, AttractionAdmin)
