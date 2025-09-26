49c5c9c206a485b68f312de85c07a6af85a8fdcc
rauf-create539 <raufimar44@gmail.com>
2025-09-26 08:09:10 +0800

    Added: models for review and ratings for user and destinations

--- backend/reviews_ratings/models.py
+++ backend/reviews_ratings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from user_authentication.models import User
+from destinations_and_attractions.models import Destination
 
 # Create your models here.
+class Review(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    reviewed_user = models.ForeignKey(User, related_name='reviews_received', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class DestinationReview(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Destination, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
