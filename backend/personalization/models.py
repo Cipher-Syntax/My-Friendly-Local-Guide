2b4a6060ff62a0f429b2376280de64cee768f539
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 11:17:32 -0700

    Added: models for personalization

--- backend/personalization/models.py
+++ backend/personalization/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user_authentication.models import User
 
 # Create your models here.
+class Personalization(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_location = models.CharField(max_length=255, blank=True, null=True)
+    travel_categories = models.JSONField(blank=True, null=True)
+    onboarding_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
