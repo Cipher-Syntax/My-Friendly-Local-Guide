5082eb91836fdde3fdcdaea6f1163f10ab08e10f
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 12:43:01 -0700

    Added: custome personalization admin

--- backend/personalization/admin.py
+++ backend/personalization/admin.py
@@ -5,6 +5,6 @@ from .models import Personalization
 class PersonalizationAdmin(admin.ModelAdmin):
     list_display = ('user', 'preferred_location', 'onboarding_completed', 'created_at')
     list_filter = ('onboarding_completed', 'preferred_location')
-    search_fields = ('user__username')
+    search_fields = ('user__username',)
 
 admin.site.register(Personalization, PersonalizationAdmin)
