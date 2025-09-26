481cba102da3ad1665861015c60807e4a1785996
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 12:40:38 -0700

    Added: custom reviews and rating for admin

--- backend/reviews_ratings/admin.py
+++ backend/reviews_ratings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review, DestinationReview
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer', 'destination', 'rating', 'timestamp')
+    list_filter = ('rating', 'timestamp')
+    search_fields = ('reviewer__username', 'reviewed_user__username')
+
+admin.site.register(Review, ReviewAdmin)
+
+class DestinationReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer', 'destination', 'rating', 'timestamp')
+    list_filter = ('rating', 'destination')
+    search_fields = ('reviewer__username', 'destination__name')
+
+admin.site.register(DestinationReview, DestinationReviewAdmin)
