4bc2f09169b04b2d0d38eb230752df4d2f35b632
aldrich030205 <aldrichzosobrado7@gmail.com>
2025-09-26 12:40:08 -0700

    Added: custom admim for report

--- backend/report/admin.py
+++ backend/report/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Report
 
 # Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('reporter', 'reported_user', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('reporter__username', 'reported_user__username')
+
+admin.site.register(Report, ReportAdmin)
