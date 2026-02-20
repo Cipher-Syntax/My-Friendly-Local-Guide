7a162245ebe6c7213dd67893169238ed8257af38
Justine Toong <programmerjustine@gmail.com>
2026-02-20 09:40:59 +0800

    Deployment Attempt 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ import os
 from datetime import timedelta
 from decouple import config, Csv #type: ignore
 from corsheaders.defaults import default_headers #type: ignore
+import dj_database_url #type: ignore
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,6 +105,7 @@ CLOUDINARY_STORAGE = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,11 +137,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        'DATABASE_URL',
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        cast=dj_database_url.parse
+    )
 }
 
 
@@ -178,6 +188,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
