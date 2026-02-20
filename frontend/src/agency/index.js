e53e7f0f92e78be0d81a174c436d0cfc5567f725
Justine Toong <programmerjustine@gmail.com>
2026-02-20 10:05:51 +0800

    fix: enforce capitalization for AgencySignin

--- frontend/src/agency/index.js
+++ frontend/src/agency/index.js
@@ -1,12 +1,12 @@
 import AgencyDashboard from "./AgencyDashboard.jsx";
 import AgencyLayout from "./AgencyLayout.jsx";
-import Agencysignin from "./Agencysignin.jsx";
+import AgencySignin from "./AgencySignin.jsx"
 import AgencyRegister from "./AgencyRegister.jsx";
 
 export {
     AgencyDashboard,
     AgencyLayout,
-    Agencysignin,
+    AgencySignin,
     AgencyRegister,
 
 }
