2054170ebd478bcc89ac0c2f0e0259d5d03faa78
Justine Toong <programmerjustine@gmail.com>
2026-03-08 12:50:06 +0800

    Updated: app url

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -149,7 +149,7 @@ const LandingPage = () => {
 
                     <div className="flex flex-col sm:flex-row justify-center gap-4">
                         <a
-                            href="https://expo.dev/accounts/cipher081505/projects/localynk/builds/d6f7caf9-6c40-45ac-a05b-63e3a1fc71fa"
+                            href="https://expo.dev/accounts/cipher081505/projects/localynk/builds/4ffdd7c7-8772-4b30-a94d-4fda64c1d83c"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="px-8 py-4 bg-white text-slate-900 rounded-xl font-bold text-lg hover:bg-slate-100 transition-all flex items-center justify-center gap-3"
