88c4774af961ea3793e50e0fab4b2a6f48107710
Justine Toong <programmerjustine@gmail.com>
2026-03-24 04:28:37 +0800

    Added: push notification

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -149,7 +149,7 @@ const LandingPage = () => {
 
                     <div className="flex flex-col sm:flex-row justify-center gap-4">
                         <a
-                            href="https://expo.dev/artifacts/eas/iMbKaof6exPbTw6GpU3ASi.apk"
+                            href="https://expo.dev/artifacts/eas/q85ftcwh1Dg3jZLkHTvQWq.apk"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="px-8 py-4 bg-white text-slate-900 rounded-xl font-bold text-lg hover:bg-slate-100 transition-all flex items-center justify-center gap-3"
